870c5e67bb9597701f4c8a37a355cd96b7304b30
jacobreppuhn <jakereppuhn@gmail.com>
2023-12-02 11:04:26 -0500

    established database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,35 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import db from './models/index';
 
 const router = express();
 
 const PORT = process.env.PORT || 8080;
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
+const NAMESPACE = 'Server';
+
+db.sequelize
+	.sync()
+	.then(() => {
+		console.log(NAMESPACE, 'Database synchronized');
+	})
+	.catch((err: Error) => {
+		console.log(NAMESPACE, 'Error synchronizing database', err);
+	});
+
+router.use((req, res, next) => {
+	console.log(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+	res.on('finish', () => {
+		console.log(
+			NAMESPACE,
+			`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+		);
+	});
+
+	next();
+});
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
