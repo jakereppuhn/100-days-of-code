745152f9c070879f876248a11048c2795895d95d
jacobreppuhn <jakereppuhn@gmail.com>
2023-12-03 18:15:28 -0500

    added auth middleware

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import db from '../models';
+import asyncHandler from 'express-async-handler';
+import { Request, Response, NextFunction } from 'express';
+
+interface ExtendedRequest extends Request {
+	user?: typeof db.User;
+}
+
+const protect = asyncHandler(async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+	if (req.cookies && req.cookies.jwt) {
+		try {
+			const token = req.cookies.jwt;
+
+			const decoded = jwt.verify(token, process.env.JWT_KEY!) as jwt.JwtPayload & { userId: string };
+
+			if (!decoded.userId) {
+				throw new Error('Invalid token');
+			}
+
+			req.user = await db.User.findByPk(decoded.userId);
+
+			next();
+		} catch (error) {
+			res.status(401).json({ message: 'Not authorized, token failed' });
+		}
+	} else {
+		res.status(401).json({ message: 'Not authorized, no token' });
+	}
+});
+
+export { protect, ExtendedRequest };
